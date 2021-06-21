d2737b1e6bebb0fd793f7e5963128c0988957d6f
hwwokim <gusdn9754@gmail.com>
2021-06-22 02:43:57 +0900

    feat: 데이터 호출해보기

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
+const PLACES = require("../data/places");
+
 const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.get("/places", (req, res, next) => {
+  res.json(PLACES);
 });
 
 module.exports = router;
