79001b8d206dc5e48a411398b71ce8a3c877a0dc
hwookim <gusdn9754@gmail.com>
2021-10-15 23:37:02 +0900

    feat: errorhandler 에서 변경된 에러 검출 및 표시

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -3,11 +3,16 @@ import {
   Middleware,
 } from "routing-controllers";
 import { NextFunction, Request, Response } from "express";
+import HttpError from "../errors/HttpError";
 
 @Middleware({ type: "after" })
 export default class ErrorHandler implements ExpressErrorMiddlewareInterface {
   error(err: Error, req: Request, res: Response, next: NextFunction): void {
     console.error(err);
+    if (err instanceof HttpError) {
+      res.status(err.status).json(err.message);
+      return;
+    }
     res.status(500).json(err);
   }
 }
